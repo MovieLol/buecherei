42f8b24ad227045b2906d6b30146589eb3bf3e0d
Martin Schulze <martin@schulzegnt.de>
2017-10-23 17:29:42 +0200

    Class ServerManager created

--- Buecherei/Buecherei/MainWindow.xaml.cs
+++ Buecherei/Buecherei/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Buecherei.classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,11 @@ namespace Buecherei
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            
         }
     }
 }
---
+++ Buecherei/Buecherei/classes/ServerManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Buecherei.classes
+{
+    class ServerManager
+    {
+        private string DBIP;
+        private int DBPort;
+        private string DBUser;
+        private string DBPass;
+        private string DBName;
+        MySqlConnection dbconnect;
+
+        public ServerManager()
+        {
+            StreamReader reader = new StreamReader("C:" + Environment.ExpandEnvironmentVariables("%HOMEPATH%") + "\\.connectionDB.cfg");
+            string connect = reader.ReadLine();
+            string[] allDataForDB = connect.Split(';');
+            this.DBIP   = allDataForDB[0];
+            this.DBPort = Convert.ToInt32(allDataForDB[1]);
+            this.DBUser = allDataForDB[2];
+            this.DBPass = allDataForDB[3];
+            this.DBName = allDataForDB[4];
+
+        }
+
+        public MySqlConnection CreateConnectionString()
+        {
+            MySqlConnectionStringBuilder con_string = new MySqlConnectionStringBuilder()
+            {
+                Server = DBIP,
+                Port = Convert.ToUInt32(DBPort),
+                UserID = DBUser,
+                Password = DBPass,
+                Database = DBName
+            };
+            dbconnect = new MySqlConnection(con_string.ToString());
+            return dbconnect;
+        }
+    }
+}
