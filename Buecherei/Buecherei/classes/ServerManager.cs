c7b41c7cddc77ca145f8ec142a590ae1a4f0598b
Martin Schulze <martin@schulzegnt.de>
2017-11-08 10:45:33 +0100

    var-name-convention_change

--- Buecherei/Buecherei/classes/ServerManager.cs
+++ Buecherei/Buecherei/classes/ServerManager.cs
@@ -5,43 +5,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+//TODO: delete unnecessary using directives 
 
 namespace Buecherei.classes
 {
     class ServerManager
     {
-        private string DBIP;
-        private int DBPort;
-        private string DBUser;
-        private string DBPass;
-        private string DBName;
-        MySqlConnection dbconnect;
+        #region Class-Attributes
+
+        private string sDBIP;
+        private int iDBPort;
+        private string sDBUser;
+        private string sDBPass;
+        private string sDBName;
+        private MySqlConnection dbconnect;
+
+        #endregion 
+
+        #region Konstruktor
 
         public ServerManager()
         {
             StreamReader reader = new StreamReader("C:" + Environment.ExpandEnvironmentVariables("%HOMEPATH%") + "\\.connectionDB.cfg");
             string connect = reader.ReadLine();
             string[] allDataForDB = connect.Split(';');
-            this.DBIP   = allDataForDB[0];
-            this.DBPort = Convert.ToInt32(allDataForDB[1]);
-            this.DBUser = allDataForDB[2];
-            this.DBPass = allDataForDB[3];
-            this.DBName = allDataForDB[4];
+            this.sDBIP   = allDataForDB[0];
+            this.iDBPort = Convert.ToInt32(allDataForDB[1]);
+            this.sDBUser = allDataForDB[2];
+            this.sDBPass = allDataForDB[3];
+            this.sDBName = allDataForDB[4];
 
         }
+
+        #endregion
+
+        #region Create Database Connection
 
         public MySqlConnection CreateConnectionString()
         {
             MySqlConnectionStringBuilder con_string = new MySqlConnectionStringBuilder()
             {
-                Server = DBIP,
-                Port = Convert.ToUInt32(DBPort),
-                UserID = DBUser,
-                Password = DBPass,
-                Database = DBName
+                Server = sDBIP,
+                Port = Convert.ToUInt32(iDBPort),
+                UserID = sDBUser,
+                Password = sDBPass,
+                Database = sDBName
             };
             dbconnect = new MySqlConnection(con_string.ToString());
             return dbconnect;
         }
+        #endregion 
     }
 }
